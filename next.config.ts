6e2ae000571687d962011bf2e8d1c5ad5ef179df
Evan Appel <appelew@gmail.com>
2026-05-10 17:06:10 -0700

    reorganized the assets into a single folder and also changed the name of the project to enki, which is an alternate name for the sumerian god, Ea.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingExcludes: {
-    "*": ["images/**", "fiction/unzipped/**"],
+    "*": ["assets/**"],
   },
 };
 
